5b1158b7c1ffb15e500615f2f6afc10ea963d154
Wandering-life <34146762+Wandering-life@users.noreply.github.com>
2018-09-13 16:16:12 +0800

    Update use_excel.py

--- use_excel.py
+++ use_excel.py
@@ -2,5 +2,5 @@
 import xlrd
 data=xlrd.open_workbook('F:\开发中心年会节目.xlsx')
 table=data.sheets()[0]
-print(table.col_values(1))
-print(table.row_values(0))
+print(table.col_values(1))  #读取第二列
+print(table.row_values(0))  #读取表头
